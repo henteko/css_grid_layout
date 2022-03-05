22189dae510dfb3f17a481dc25eb65272d419abd
henteko <henteko07@gmail.com>
2022-03-05 08:50:57 +0000

    レスポンシブ対応

--- dot/index.js
+++ dot/index.js
@@ -6,5 +6,5 @@ for (let step = 0; step < 2500; step++) {
   const div = document.createElement("div");
   div.className = "dot";
   div.addEventListener("click", draw);
-  document.querySelector("#editor").append(div);
+  document.querySelector("#dot-area").append(div);
 }
